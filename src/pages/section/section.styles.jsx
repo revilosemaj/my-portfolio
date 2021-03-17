4606ac0bd193ec324881b014704e4d586f195b69
Oliver Aco <benavidez.jemver@gmail.com>
2021-03-18 06:40:06 +0800

    add style in section

--- src/pages/section/section.styles.jsx
+++ src/pages/section/section.styles.jsx
@@ -1,8 +1,21 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+const columnStyle = css`
+    float: left;
+    _display: inline;
+    width:100%;
+    text-align: center;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
 
-const columnStyles = `
-    
+    @media screen and (min-width: 30em) and (max-width: 60em) {
+        width: 50%;
+    }
+
+    @media screen and (min-width: 60em) {
+        width: 25%;
+    }
 `;
+
 export const SectionContainer = styled.div`
     *zoom: 1;
 
@@ -14,6 +27,10 @@ export const SectionContainer = styled.div`
     &:after {
         clear: both;
     }
+`;
+
+export const ColumnOne = styled.div`
+    ${columnStyle}
 `;
 
  
